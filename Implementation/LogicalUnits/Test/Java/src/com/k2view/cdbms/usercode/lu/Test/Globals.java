2c6b8d8cabcd35283b7604d15395d18726149c73
zz-kforce2 <zz-kforce2@cloud.k2view.com>
2025-03-06 11:30:56 +0000

    second commit+ add global

---
+++ Implementation/LogicalUnits/Test/Java/src/com/k2view/cdbms/usercode/lu/Test/Globals.java
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////////////
+// LU Globals
+/////////////////////////////////////////////////////////////////////////
+
+package com.k2view.cdbms.usercode.lu.Test;
+
+import com.k2view.cdbms.usercode.common.SharedGlobals;
+import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
+
+public class Globals extends SharedGlobals { 
+    public static String test="false";
+
+
+	
+
+
+}
