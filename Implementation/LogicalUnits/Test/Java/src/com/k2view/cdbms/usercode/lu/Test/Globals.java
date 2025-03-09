83da1b47c69f60f9101170ef31da63fe24a81ea1
zz-kforce2 <zz-kforce2@cloud.k2view.com>
2025-03-09 10:21:44 +0000

    ADD TWO GLOBALS

--- Implementation/LogicalUnits/Test/Java/src/com/k2view/cdbms/usercode/lu/Test/Globals.java
+++ Implementation/LogicalUnits/Test/Java/src/com/k2view/cdbms/usercode/lu/Test/Globals.java
@@ -8,7 +8,9 @@ import com.k2view.cdbms.usercode.common.SharedGlobals;
 import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
 
 public class Globals extends SharedGlobals { 
-    public static String test="false" 
+    public static String test="false" ;
+
+    public static String test2="false" ;
 
 
 
