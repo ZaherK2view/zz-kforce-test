fa3b739e768c6a37f038973fdacce429c8bdc830
zz-kforce2 <zz-kforce2@cloud.k2view.com>
2025-03-09 10:04:30 +0000

    add another error with no deploy

--- Implementation/LogicalUnits/Test/Java/src/com/k2view/cdbms/usercode/lu/Test/Globals.java
+++ Implementation/LogicalUnits/Test/Java/src/com/k2view/cdbms/usercode/lu/Test/Globals.java
@@ -8,7 +8,7 @@ import com.k2view.cdbms.usercode.common.SharedGlobals;
 import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
 
 public class Globals extends SharedGlobals { 
-    public static String test="false";
+    public static String test="false";;
 
 
 
